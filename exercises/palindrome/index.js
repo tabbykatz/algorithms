b3121903dee16461b708eea71a1faf4a039e7ccd
tabbykatz <tomelay@gmail.com>
2021-05-25 12:27:26 -0700

    add reversestr && palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,17 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+//    return reversed(str) === str;
+//
+//
+//}
 
+//function reversed(str) {
+//    return str.split('').reduce((rev, char) => char + rev, '');
+return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1];
+});
+
+}
 module.exports = palindrome;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,40 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    //solution 1
+
+    //let ourString = str;
+    //let anArray = ourString.split('');
+    //let revArray = anArray.reverse();
+    //let answer = revArray.join('');
+    //return answer;
+
+    //solution 1 (clean)
+
+    //return str.split('').reverse().join('');
+
+    //solution 2
+
+   //let answer = ""
+   //for (let i = (str.length - 1); i >= 0; i--) {
+   //    answer += str[i];
+   // }
+   // return answer
+
+   //solution 2 (clean)
+   //let reversed = '';
+
+   //for (let character of str) {
+       //reversed = character + reversed;
+   //}
+   //return reversed;
+
+  //solution 3
+    return str.split('').reduce((rev, char) => char + rev, '');
+
+
+
+}
 
 module.exports = reverse;
