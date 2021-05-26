7d89f4b3cb31e5aa92702c4837944c0893f082ac
tabbykatz <tomelay@gmail.com>
2021-05-26 11:07:35 -0700

    Add exercises

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,19 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+
+function reverseInt(n) {
+    let neg = false;
+    if (n < 0) {
+        neg = true;
+        n *= -1;
+    }
+    let strN = n.toString()
+    let revN = strN.split('').reduce((rev, char) => char + rev, '');
+
+    //console.log(strN)
+    //console.log(revN)
+    return neg ? (Number(revN) * -1) : (Number(revN));
+}
 
 module.exports = reverseInt;
