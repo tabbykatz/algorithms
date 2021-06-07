363e63b2116e5a023865370baa1b4217ed622999
tabbykatz <tomelay@gmail.com>
2021-06-07 11:06:48 -0700

    add steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,37 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+
+//recursive
+    //base case
+    if (row === n) {
+        return;
+    }
+    if (stair.length === n) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+    // if (stair.length <= row) {
+    //     stair += "#";
+    // } else {
+    //     stair += " ";
+    stair.length <= row ? stair += "#" : stair += " ";
+    steps(n, row, stair);
+}
+//iterative
+//     for (i = 0; i < n; i++) {
+//         let stair = "";
+//         for (let j = 0; j < n; j++) {
+//             if (j <= i) {
+//                 stair += "#";
+//             } else {
+//                 stair += " ";
+//             }
+//         }
+//     console.log(stair)
+//     }
+// }
 
 module.exports = steps;
+
